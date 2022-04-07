5c4a952ffc5272680b4156364abc54f3c670bc32
wqicheng <wqicheng@foxmail.com>
2022-04-07 10:17:04 +0800

    [FIXBUG] getImgURL:[2022年 04月 07日 星期四 10:17:04 CST]

--- index.js
+++ index.js
@@ -26,9 +26,7 @@
        //let imgStr='url("https://s.cn.bing.net/th?id=OHR.Malaga_ZH-CN9644862917_1920x1080.jpg&rf=LaDigue_1920x1080.jpg")'
        let reg=/"(https?:\/\/[\w.]+\/?\S*)"/
        let res=reg.exec(imgStr)
-       if(res){
-        let url=res?.[1]||""
-        return url
-       }
+       let url=res?.[1]||""
+       return url
     }
 })();
